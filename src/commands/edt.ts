2a6b8c41bd44726792509ac0fcf1dfa92f560c59
Logan TANN <logan@kagescan.fr>
2022-01-19 00:43:34 +0000

    Feat: amélioration + ajout arguments Groupe et Date

--- src/commands/edt.ts
+++ src/commands/edt.ts
@@ -6,10 +6,10 @@ import { createCommand } from "./mod.ts";
 interface embedField {
   name: string,
   value: string,
-  inline: boolean
+  inline: boolean,
+  _dateForSort: Date
 };
 
-const group = "209";
 function findEdt(group: string, groupList: descalendrierGroupes): descalendrierEdt[] {
   for (const category of groupList) {
     for (const catgroup of category.groupes) {
@@ -19,21 +19,23 @@ function findEdt(group: string, groupList: descalendrierGroupes): descalendrierE
     }
   }
 }
-async function getResult(): Promise<Array<embedField>>{
-  const edtList: descalendrierEdt[] = findEdt(group, await getDescalendrier());
+
+async function getResult(edtList: descalendrierEdt[], dateToSee: Date): Promise<Array<embedField>>{
   let result = [];
   for (const edt of edtList) {
     const begin: Date = new Date(edt.start);
+    if (begin.getMonth() != dateToSee.getMonth() || begin.getDate() != dateToSee.getDate()) continue;
     const hour = helpers.dateToDiscordTimestamp(begin, helpers.DiscordTimestampFlag.time);
-    const tmpdate = helpers.dateToDiscordTimestamp(begin, helpers.DiscordTimestampFlag.date);
     const teacher = helpers.pseudonymizeTeacher(edt.enseignant);
     const location = helpers.abbreviateBlockList(edt.location);
     result.push({
-      name: edt.name + ' ' + tmpdate,
-      value: `**Heure**: ${hour}${teacher ? "\n**Ens**: "+teacher : ""}\n**Salle**: ${location}`,
-      inline: true
+      name: edt.name,
+      value: `**Heure**: ${hour}\n**Salle**: ${location}${teacher ? "\n**Ens**: " + teacher : ""}`,
+      inline: true,
+      _dateForSort: begin
     })
   }
+  result.sort((a, b) => a._dateForSort.getTime() - b._dateForSort.getTime());
   return result;
 }
 
@@ -41,17 +43,55 @@ createCommand({
   name: "edt",
   description: "Obtenez l'EDT depuis l'API de Descalendrier",
   type: ApplicationCommandTypes.ChatInput,  
-
-  arguments: [
-
+  options: [
+    {
+      type: 3,
+      name: "groupe",
+      description: "Le nom du groupe à afficher",
+      required: true, 
+      choices: [
+        { name: "Groupe 101", value: "101" }, { name: "Groupe 102", value: "102" }, 
+        { name: "Groupe 103", value: "103" }, { name: "Groupe 104", value: "104" }, 
+        { name: "Groupe 105", value: "105" }, { name: "Groupe 106", value: "106" }, 
+        { name: "Groupe 107", value: "107" }, { name: "Groupe 108", value: "108" }, 
+        { name: "Groupe 109", value: "109" }, { name: "Groupe 101", value: "110" }, 
+        { name: "Groupe 111", value: "111" }, { name: "Groupe 112", value: "112" },
+        { name: "Groupe 201", value: "201" }, { name: "Groupe 202", value: "202" },
+        { name: "Groupe 203", value: "203" }, { name: "Groupe 204", value: "204" },
+        { name: "Groupe 205", value: "205" }, { name: "Groupe 206", value: "206" },
+        { name: "Groupe 207", value: "207" }, { name: "Groupe 208", value: "208" },
+        { name: "Groupe 209", value: "209" }, { name: "Groupe APP", value: "Apprentissage" },
+      ],
+    },
+    {
+      type: 3,
+      name: "date",
+      description: "mettez n'imp si vous voulez la date de demain plutôt qu'aujourd'hui",
+      required: false, 
+      choices: [
+        { name: "demain", value: "dm" }
+      ],
+    }
   ],
 
-  execute: async (Bot, interaction) => {
-    let result;
+  execute: async (Bot, interaction, arg ) => {
+    let embedOut;
     try {
-      result = await getResult();
+      const group: string = interaction.data.options[0].value;
+      const edtList: descalendrierEdt[] = findEdt(group, await getDescalendrier());
+      let dateToSee = new Date();
+      if (interaction.data.options.lenght > 1) {
+        dateToSee.setDate(dateToSee.getDate() + 1);
+      }
+      embedOut = {
+        title: `Emploi du Temps des ${group} pour le ${helpers.dateToDiscordTimestamp(dateToSee, helpers.DiscordTimestampFlag.date)}`,
+        type: "rich",
+        url: "https://edt.bde-faction.fr/",
+        author: { name: "Descalendrier pour Discord", iconUrl: "https://edt.bde-faction.fr/favicon.png" },
+        fields: await getResult(edtList, dateToSee)
+      };
     } catch (error) {
-      result = [{name: "Error", value: error, inline: false}];
+      embedOut = {name: "Error", value: error, inline: false};
     }
 
     await Bot.helpers.sendInteractionResponse(
@@ -60,14 +100,7 @@ createCommand({
       {
         type: InteractionResponseTypes.ChannelMessageWithSource,
         data: {
-          content: `tmp`,
-          embeds: [{
-            title: "EDT",
-            type: "rich",
-            url: "https://edt.bde-faction.fr/",
-            author: "Descalendrier for Discord",
-            fields: result
-          }]
+          embeds: [embedOut]
         }
       }
     );
